f02d60c8749f4c60ea96f59c6a0f1433e9c39432
dennimus <dennimus@live.nl>
2018-09-20 19:44:57 +0200

    buildtests

--- Assets/BuildScript/Editor/BuildScript.cs
+++ Assets/BuildScript/Editor/BuildScript.cs
@@ -6,7 +6,7 @@ class BuildScript {
 	static string[] SCENES = FindEnabledEditorScenes();
 
 	static string APP_NAME = "polygon";
-	static string TARGET_DIR = "\builds";
+	static string TARGET_DIR = "/buildsTest";
 
 	static void PerformAllBuilds ()
 	{
